dfdf6af56f6662b8346d777cba3fd9ae3a4bdc7d
Chimera <j.calderon3@gmail.com>
2021-01-12 20:18:27 -0500

    Routes script updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 
+let notes = [];
+
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
-        res.json(getNotes);
+        notes = fs.readFileSync(path.resolve("..db/db.json"), "utf8");
+        notes = JSON.parse(notes);
+        return res.json(getNotes);
+
     });
-    // app.post("/api/notes", function(req, res) {
-        res.json(getNotes);
-    // });
+
+    app.post("/api/notes", function(req, res) {
+        notes = fs.readFileSync(path.resolve("..db/db.json"), "utf8");
+        notes = JSON.parse(notes);
+        return res.json(getNotes);
+    });
+
+    app.delete("/api/notes", function(req, res) {
+        notes = fs.readFileSync(path.resolve("..db/db.json"), "utf8");
+        notes = JSON.parse(notes);
+        fs.writeFile(path.resolve("..db/db.json"), notes, function(err) {
+            if(err) throw err;
+        });
+
+        res.json(JSON.parse(notes));
+
+    });
+
 };
