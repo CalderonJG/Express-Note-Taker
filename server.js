e48d4665b7bd1b5262ace24344e92cd8af984191
Chimera <j.calderon3@gmail.com>
2021-01-12 20:25:47 -0500

    Server script updated

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // Tells node that we are creating an "express" server
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 //Sets up the Express app to handle data parsing
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
