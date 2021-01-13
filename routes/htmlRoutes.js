a5589c38b5f9ac29a5d19a62504470a4826ce583
Chimera <j.calderon3@gmail.com>
2021-01-12 20:23:00 -0500

    Updated HTML routes script

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,15 @@
 
 const path = require("path");
 
-module.exports
+module.exports = function(app) {
 
 // HTML GET requests
-app.get("/index", function(req, res) {
-    res.sendFile(path.join(_dirname, "../assets/index.html"));
-});
+    app.get("/index", function(req, res) {
+        res.sendFile(path.join(_dirname, "../assets/index.html"));
+    });
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(_dirname, "../assets/notes.html"));
-});
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(_dirname, "../assets/notes.html"));
+    });
+
+};
